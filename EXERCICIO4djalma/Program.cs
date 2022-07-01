316ea4fc470f7d30ebcf00cfe6df1b82727581cd
fé <f@YAGO>
2022-06-30 21:00:59 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCICIO4djalma/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO4djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+           
+
+            for (n = 1; n < 51; n++) 
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
